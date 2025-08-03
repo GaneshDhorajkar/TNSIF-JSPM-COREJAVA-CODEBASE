af56016579bdb8b36d2821efab2b899a2dc888f4
GaneshDhorajkar <ganeshdhorajkar007@gmail.com>
2025-08-03 19:53:22 +0530

    COMMITFIRST.

---
+++ firstday/src/firstday/LOOP.java
@@ -0,0 +1,10 @@
+package firstday;
+
+public class LOOP {
+
+	public static void main(String[] args) {
+		   for (int i = 1; i <= 7; i++) {
+	            System.out.println("NUMBER: " + i);
+	}
+	}
+}
